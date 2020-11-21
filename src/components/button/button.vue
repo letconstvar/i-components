ea438a4d0ce02b4f567f18e80e7fcb827ee978b5
zhouzhu <var.zhou@qq.com>
2020-11-22 02:59:06 +0800

    add button component

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,5 +1,17 @@
 <template>
-  <button class="i-button" :class="[`i-button--${type}`]">
+  <button 
+    class="i-button" 
+    :class="[
+      `i-button--${type}`,
+      { 'is-plain': plain },
+      { 'is-round': round },
+      { 'is-circle': circle },
+      { 'is-disabled': disabled },
+      `i-button--${size}`,
+    ]"
+    :disabled="disabled"
+    @click="handleClick"
+  >
     <span><slot></slot></span>
   </button>
 </template>
@@ -10,11 +22,31 @@
       type: {
         type: String,
         default: ''
+      },
+      plain: {
+        type: Boolean,
+        default: false
+      },
+      round: {
+        type: Boolean,
+        default: false
+      },
+      circle: {
+        type: Boolean,
+        default: false
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      size: {
+        type: String,
+        default: 'medium'
       }
     },
-    data () {
-      return {
-
+    methods: {
+      handleClick (e) {
+        this.$emit('click', e);
       }
     }
   }
@@ -42,7 +74,7 @@
     -moz-user-select: none;
     -webkit-user-select: none;
     -ms-user-select: none;
-    padding: 12px 20px;
+    /* padding: 12px 20px; */
     font-size: 14px;
     border-radius: 4px; 
   }
@@ -65,7 +97,6 @@
     border-color: #66b1ff;
     color: #ffffff;
   }
-
   /* success */
   .i-button--success {
     color: #ffffff;
@@ -78,7 +109,6 @@
     border-color: #85ce61;
     color: #ffffff;
   }
-
   /* info */
   .i-button--info {
     color: #ffffff;
@@ -104,7 +134,6 @@
     border-color: #ebb563;
     color: #ffffff;
   }
-
   /* danger */
   .i-button--danger {
     color: #ffffff;
@@ -117,4 +146,107 @@
     border-color: #f78989;
     color: #ffffff;
   }
+
+  /* plain */
+  .i-button.is-plain:hover,
+  .i-button.is-plain:focus {
+    background: #ffffff;
+    border-color: #409eff;
+    color: #409eff;
+  }
+  /* plain primary */
+  .i-button--primary.is-plain {
+    color: #a2a8ad;
+    background: #ecf5ff;
+    border-color: #b3d8ff;
+  }
+  .i-button--primary.is-plain:hover,
+  .i-button--primary.is-plain:focus {
+    color: #ffffff;
+    background: #409eff;
+    border-color: #409eff;
+  }
+  /* plain success */
+  .i-button--success.is-plain {
+    color: #67c23a;
+    background: #f0f9eb;
+    border-color: #c2e7b0;
+  }
+  .i-button--success.is-plain:hover,
+  .i-button--success.is-plain:focus {
+    color: #ffffff;
+    background: #67c23a;
+    border-color: #67c23a;
+  }
+  /* plain info */
+  .i-button--info.is-plain {
+    color: #909399;
+    background: #f4f4f5;
+    border-color: #d3d4d6;
+  }
+  .i-button--info.is-plain:hover,
+  .i-button--info.is-plain:focus {
+    color: #ffffff;
+    background: #909399;
+    border-color: #909399;
+  }
+  /* plain warning */
+  .i-button--warning.is-plain {
+    color: #e6a23c;
+    background: #fdf6ec;
+    border-color: #f5dab1;
+  }
+  .i-button--warning.is-plain:hover,
+  .i-button--warning.is-plain:focus {
+    color: #ffffff;
+    background: #e6a23c;
+    border-color: #e6a23c;
+  }
+  /* plain danger */
+  .i-button--danger.is-plain {
+    color: #f56c6c;
+    background: #fef0f0;
+    border-color: #fbc4c4;
+  }
+  .i-button--danger.is-plain:hover,
+  .i-button--danger.is-plain:focus {
+    color: #ffffff;
+    background: #f56c6c;
+    border-color: #f56c6c;
+  }
+
+  /* round */
+  .i-button.is-round {
+    border-radius: 20px;
+    padding: 12px 23px;
+  }
+
+  /* circle */
+  .i-button.is-circle {
+    border-radius: 50%;
+    padding: 12px;
+  }
+
+  /* disabled */
+  .i-button.is-disabled:hover {
+    color: #c0c4cc;
+    cursor: not-allowed;
+    background-color: #ffffff;
+    border-color: #ebeef5;
+  }
+
+  /* size */
+  .i-button--mini {
+    padding: 7px 15px;
+    font-size: 12px;
+  }
+  .i-button--medium {
+    padding: 9px 15px;
+    font-size: 12px;
+  }
+  .i-button--bigger {
+    padding: 10px 20px;
+    font-size: 14px;
+  }
+
 </style>
--- src/views/button.vue
+++ src/views/button.vue
@@ -1,11 +1,53 @@
 <template>
   <div class="row">
-    <i-button>button</i-button>
-    <i-button type="primary">primary</i-button>
-    <i-button type="success">plain</i-button>
-    <i-button type="warning">warning</i-button>
-    <i-button type="info">info</i-button>
-    <i-button type="danger">danger</i-button>
+    <div>
+      <div>type</div>
+      <i-button>button</i-button>
+      <i-button type="primary">primary</i-button>
+      <i-button type="success">plain</i-button>
+      <i-button type="warning">warning</i-button>
+      <i-button type="info">info</i-button>
+      <i-button type="danger">danger</i-button>
+    </div>
+    <br>
+    <div>
+      <div>plain</div>
+      <i-button plain type="primary">primary</i-button>
+      <i-button plain type="success">plain</i-button>
+      <i-button plain type="warning">warning</i-button>
+      <i-button plain type="info">info</i-button>
+      <i-button plain type="danger">danger</i-button>
+    </div>
+    <br>
+    <div>
+      <div>round</div>
+      <i-button plain round type="success">plain</i-button>
+      <i-button plain round type="primary">primary</i-button>
+      <i-button plain round type="warning">warning</i-button>
+      <i-button plain round type="info">info</i-button>
+      <i-button plain round type="danger">danger</i-button>
+    </div>
+    <br>
+    <div>
+      <div>circle</div>
+      <i-button circle plain round type="success"><i-icon name="search"></i-icon></i-button>
+      <i-button circle plain round type="primary"><i-icon name="41shangchuantupian"></i-icon></i-button>
+      <i-button circle plain round type="warning"><i-icon name="41shangchuantupian"></i-icon></i-button>
+      <i-button circle plain round type="info"><i-icon name="41shangchuantupian"></i-icon></i-button>
+      <i-button circle plain round type="danger" @click="fn"><i-icon name="41shangchuantupian"></i-icon></i-button>
+    </div>
+    <br>
+    <div>
+      <div>size</div>
+      <i-button size="mini" plain type="success"><i-icon name="search"></i-icon></i-button>
+      <i-button size="medium" plain type="primary"><i-icon name="41shangchuantupian"></i-icon></i-button>
+      <i-button size="bigger" plain type="warning"><i-icon name="41shangchuantupian"></i-icon></i-button>
+    </div>
+    <br>
+    <div>
+      <div>disabled</div>
+      <i-button circle plain disabled round type="danger" @click="fn"><i-icon name="41shangchuantupian"></i-icon></i-button>
+    </div>
   </div>
 </template>
 
@@ -19,6 +61,11 @@
     data () {
       return {
 
+      }
+    },
+    methods: {
+      fn () {
+        console.log(0)
       }
     }
   }
