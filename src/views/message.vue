77f7383e41879c95804282d712e195ba2d921806
zhouzhu <var.zhou@qq.com>
2020-12-24 10:14:37 +0800

    bug fix: message component

--- src/views/message.vue
+++ src/views/message.vue
@@ -1,18 +1,47 @@
 <template>
   <div>
-    <button @click="openMessage()">message</button>
+    <i-button @click="successMessage">success</i-button>
+    <i-button @click="infoMessage">info</i-button>
+    <i-button @click="warningMessage">warning</i-button>
+    <i-button @click="errorMessage">error</i-button>
   </div>
 </template>
 
 <script>
+  import button from '@/components/button/button';
   export default {
+    components: {
+      [button.name]: button
+    },
     methods: {
-      openMessage () {
+      successMessage () {
         this.$message({
-          type:'success',
-          message:'11111111'
+          type: 'success',
+          text: '这是一条success消息',
+          duration: 4000
         })
-      }
+      },
+      infoMessage () {
+        this.$message({
+          type: 'info',
+          text: '这是一条info消息',
+          duration: 4000
+        })
+      },
+      warningMessage () {
+        this.$message({
+          type: 'warning',
+          text: '这是一条warning消息',
+          duration: 4000
+        })
+      },
+      errorMessage () {
+        this.$message({
+          type: 'error',
+          text: '这是一条error消息',
+          duration: 4000
+        })
+      },
     }
   }
 </script>
